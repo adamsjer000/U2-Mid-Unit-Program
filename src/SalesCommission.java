54cda50cadc5d045dd7564e77f32543feac4f72f
adamsjer000 <adamsjer000@warren.k12.in.us>
2021-09-21 12:54:57 -0400

    Half Done

--- src/SalesCommission.java
+++ src/SalesCommission.java
@@ -1,27 +1,60 @@
-/*
-A car salesperson typically earns an hourly wage and a commission on any sales they completed. They typically work 30 to 40
-hours a week, earning $5.25 to $7.50 an hour. Additionally, they earn 1% to 3% of all sales they've completed that week,
-which a typical car sells for $10,000 to $30,000. A salesperson with a 1% commission that sells a car for $20,000 earns $200.00.
+import javax.swing.*;
+import java.text.DecimalFormat;
 
-Write a program which prompts the user to enter the hourly rate, hours worked, the sales commission percent and the total
-sales of a salesperson and calculates their total earnings. The program should include multiple methods. (What ARE the
-smaller pieces?)
+// A car salesperson typically earns an hourly wage and a commission on any sales they completed. They typically work 30 to 40
+// hours a week, earning $5.25 to $7.50 an hour. Additionally, they earn 1% to 3% of all sales they've completed that week,
+// which a typical car sells for $10,000 to $30,000. A salesperson with a 1% commission that sells a car for $20,000 earns $200.00.
+//
+// Write a program which prompts the user to enter the hourly rate, hours worked, the sales commission percent and the total
+// sales of a salesperson and calculates their total earnings. The program should include multiple methods. (What ARE the
+// smaller pieces?)
+//
+// Example:
+//
+// Hourly Rate: 6.25
+// Hours Worked: 33.5
+// Sales Commission: 1.5%
+// Total Sales: $65,000.00
+//
+// Total Earnings: $1184.38
+//
+     /* THE PLAN
 
-Example:
+         1. Get the inputs
+         2. Calculate the hourly rate
+         3. Calculate the hours worked
+         4. Calculate the sales commission percent
+         5. Calculate the total sales of a salesperson and their total earnings
+         6. Display results/output
 
-Hourly Rate: 6.25
-Hours Worked: 33.5
-Sales Commission: 1.5%
-Total Sales: $65,000.00
-
-Total Earnings: $1184.38
-
- */
-
+     */
 public class SalesCommission {
 
     public static void main(String[] args) {
 
+        double hourlyPay = getInput("How many hours did you work ?");
+        double carSales = getInput("How many cars did you sale ?");
+        double salesCommission = getInput("What is the sales commission");
+        double hourlyRate = getInput("How much do you get paid a hour ?");
     }
 
+
+
+    public static double getInput(String prompt){
+       return Double.parseDouble(JOptionPane.showInputDialog(prompt));
+ }
+   public static double hourlyPay (double hoursWorked, double hourlyRate){
+        return  hoursWorked + hourlyRate;
+   }
+   public static double commission (double carSales, double salesCommission){
+        return carSales + salesCommission;
+   }
+   public static double totalPay (double hourlyPay, double commission){
+        return hourlyPay + commission;
+   }
+   public static void output(double getSales){
+        DecimalFormat round = new DecimalFormat("#,###.00");
+
+        JOptionPane.showMessageDialog(null, "Total Sales :" + round.format(getSales);
+   }
 }
